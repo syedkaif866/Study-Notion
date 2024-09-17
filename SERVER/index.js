22a163f7329e2ca0d3400c9cfb702723c64e1bbd
syedkaif866 <161060352+syedkaif866@users.noreply.github.com>
2024-09-17 15:39:28 +0530

    Update index.js

--- SERVER/index.js
+++ SERVER/index.js
@@ -7,7 +7,6 @@ const paymentRoutes = require("./routes/Payments");
 const courseRoutes = require("./routes/Course");
 const contactUsRoute = require("./routes/Contact");
 const database = require("./config/database");
-const cors = require('cors');
 const cookieParser = require("cookie-parser");
 const {cloudinaryConnect } = require("./config/cloudinary");
 const fileUpload = require("express-fileupload");
@@ -21,12 +20,7 @@ database.connect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
-	})
-)
+
 
 
 app.use(
