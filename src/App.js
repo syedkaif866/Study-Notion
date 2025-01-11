86a0ce866cb8c1b9e8dff04b4235b3195e908442
Syed Kaif <161060352+syedkaif866@users.noreply.github.com>
2025-01-11 12:30:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ function App() {
         />  
 
         <Route
-          path="reset-password/:id"
+          path="update-password/:id"
           element={
             <OpenRoute>
               <UpdatePassword />
